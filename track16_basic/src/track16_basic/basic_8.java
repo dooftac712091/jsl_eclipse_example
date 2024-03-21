b8fe1ada17abdb90f3cebaadd6a2e333ba561589
seungholee <seungholee@192.168.0.7>
2024-03-21 15:13:15 +0900

    클래스의 객체 생성 및 데이터 스트림 문자 입출력 정

--- track16_basic/src/track16_basic/basic_8.java
+++ track16_basic/src/track16_basic/basic_8.java
@@ -2,30 +2,29 @@ package track16_basic;
 
 import java.util.Scanner;
 
-public class basic_7 {
+public class basic_8 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int kor = 0;
 		int mat = 0;
+		int eng = 0;
 		int total = 0;
 		
+		String name;
+		
 		System.out.print("お名前を入力してください：");
-		String name = sc.next();
-		System.out.println(name + "さん、おはようございます。");
-		
+		name = sc.next();
 		System.out.print("国語の点数を入力してください：");
-		kor = sc.nextInt(); 
-		
+		kor = sc.nextInt();
 		System.out.print("数学の点数を入力してください：");
-		mat = sc.nextInt(); 
+		mat = sc.nextInt();
+		System.out.print("英語の点数入力してください：");
+		eng = sc.nextInt();
 		
-		total = kor + mat;
+		total = kor + mat + eng;
 		
 		System.out.println(name + "さんの総点：" + total);
-		System.out.println(name + "さんの平均：" + total / 2);
-		
-		sc.close();
 
 	}
 }
